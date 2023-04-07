06f04fd95209e81de7f2d2ef1a94528096817a91
Quirin Sailer <quirin.sailer@loew-sailer.de>
2023-04-07 17:14:08 +0200

    :card_file_box: Added Migration for ScrimMatchreportInfantery

--- squittal.ScrimPlanetmans.App/Migrations/20230121213426_AddRevives.cs
+++ squittal.ScrimPlanetmans.App/Migrations/20230121213426_AddRevives.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -8,6 +11,36 @@ namespace squittal.ScrimPlanetmans.App.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            /*
+        public string ScrimMatchId { get; set; }
+        public int ScrimMatchRound { get; set; }
+        public int TeamOrdinal { get; set; }
+        public int FacilityCapturePoints { get; set; }
+
+        public int GrenadeAssists { get; set; }
+        public int SpotAssists { get; set; }
+
+        public int GrenadeTeamAssists { get; set; }
+        
+        */
+            migrationBuilder.CreateTable(
+                name: "ScrimMatchReportInfantryTeamRoundStats",
+                columns: table => new
+                {
+                    ScrimMatchId = table.Column<string>(nullable: false),
+                    ScrimMatchRound = table.Column<string>(nullable: false),
+                    TeamOrdinal = table.Column<string>(nullable: false),
+                    FacilityCapturePoints = table.Column<string>(nullable: false),
+                    GrenadeAssists = table.Column<int>(nullable: false),
+                    SpotAssists = table.Column<int>(nullable: false),
+                    GrenadeTeamAssists = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    //table.PrimaryKey("PK_Faction", x => x.Id);
+                });
+            
+            /*
             migrationBuilder.AddColumn<int>(
                 name: "Revived",
                 table: "ScrimMatchReportInfantryTeamRoundStats",
@@ -30,14 +63,14 @@ namespace squittal.ScrimPlanetmans.App.Migrations
                 name: "Revives",
                 table: "ScrimMatchReportInfantryPlayerRoundStats",
                 nullable: false,
-                defaultValue: 0);
+                defaultValue: 0);*/
 
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(name: "Revived", table: "ScrimMatchReportStats");
-            migrationBuilder.DropColumn(name: "Revives", table: "ScrimMatchReportStats");
+            //migrationBuilder.DropColumn(name: "Revived", table: "ScrimMatchReportStats");
+            //migrationBuilder.DropColumn(name: "Revives", table: "ScrimMatchReportStats");
         }
 
     }
