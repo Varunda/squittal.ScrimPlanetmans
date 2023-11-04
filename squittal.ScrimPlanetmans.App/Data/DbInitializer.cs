1cc7980f78edd10b18e742a44a04f13347c7306b
Varunda <trvarunda@gmail.com>
2023-11-04 13:32:30 -0700

    add debug to db initialzer

--- squittal.ScrimPlanetmans.App/Data/DbInitializer.cs
+++ squittal.ScrimPlanetmans.App/Data/DbInitializer.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace squittal.ScrimPlanetmans.Data
 {
-    public static class DbInitializer
-    {
-        public static void Initialize(IServiceProvider serviceProvider)
-        {
+    public class DbInitializer {
+
+        public static void Initialize(IServiceProvider serviceProvider) {
+            ILogger<DbInitializer> logger = serviceProvider.GetRequiredService<ILogger<DbInitializer>>();
 
             using (var context = new PlanetmansDbContext(
                 serviceProvider.GetRequiredService<
                 DbContextOptions<PlanetmansDbContext>>()))
             {
+
+                logger.LogInformation($"connStr: {context.Database.GetConnectionString()}");
                 context.Database.Migrate();
                 return;
             }
